3be22143bf2049d3360c8cb38be1a97e2e67a993
Apoorv Singh <apoorv.singh15@gmail.com>
2019-07-27 22:49:53 +0530

    added style changes

--- src/containers/Quotes.jsx
+++ src/containers/Quotes.jsx
@@ -64,7 +64,6 @@ const Quotes = ({ location: { state = {} } }) => {
                 style={{
                   fontSize: largeTextInputValue,
                   position: "relative",
-
                   top: "50%",
                   left: "50%",
                   transform: "translate(-50%, -50%)"
